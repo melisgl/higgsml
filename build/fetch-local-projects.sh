81d6bfb715c5bb9a2a6daf5ad4c83f8f0c5a5c53
Gábor Melis <mega@retes.hu>
2015-01-21 10:47:48 +0100

    fix fetching of dependencies
    - use public git url
    - don't rely on the 'git co' alias

--- build/fetch-local-projects.sh
+++ build/fetch-local-projects.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
-git clone git@github.com:melisgl/mgl.git
-(cd mgl && git co higgsml)
+git clone git://github.com/melisgl/mgl.git
+(cd mgl && git checkout higgsml)
 
 git clone https://github.com/melisgl/mgl-mat
-(cd mgl-mat && git co higgsml)
+(cd mgl-mat && git checkout higgsml)
 
 git clone https://github.com/melisgl/cl-cuda
-(cd cl-cuda && git co higgsml)
+(cd cl-cuda && git checkout higgsml)
 
 git clone https://github.com/takagi/cl-reexport/
-(cd cl-reexport && git co e49847e14d57a7ce91d82fb9964166f3649587f0)
+(cd cl-reexport && git checkout e49847e14d57a7ce91d82fb9964166f3649587f0)
 
 git clone https://github.com/arielnetworks/cl-pattern/
-(cd cl-pattern && git co 4717b690d21a3388bec3a4a271d2dcb7cdb561ce)
+(cd cl-pattern && git checkout 4717b690d21a3388bec3a4a271d2dcb7cdb561ce)
